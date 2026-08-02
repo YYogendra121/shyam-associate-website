ed2a51d532be70cf353ed508b6e206d66d7b5254
Yugdev chhetri <yogeekhadka8@gmail.com>
2026-08-02 16:57:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@
           <h4>Get in Touch</h4>
           <p><i class="fa-solid fa-phone" style="color:#c9a961;margin-right:.5rem"></i> +91 73650 28541</p>
           <p><i class="fa-solid fa-envelope" style="color:#c9a961;margin-right:.5rem"></i> shyamassociateco@gmail.com</p>
-          <p><i class="fa-solid fa-location-dot" style="color:#c9a961;margin-right:.5rem"></i> G-BLOCK EXTEN, NUTAN PALLY BENACHITY DURGAPUR, WARD NO.14<br/>West Bengal, India, Code:19</p>
+          <p><i class="fa-solid fa-location-dot" style="color:#c9a961;margin-right:.5rem"></i> G-BLOCK EXTEN, NUTAN PALLY BENACHITY DURGAPUR, WARD NO.14<br>Pincode:Pincode 713213<br/>West Bengal, India</p>
           <p><a href="https://maps.google.com/?q=G-BLOCK+EXTEN%2C+NUTAN+PALLY+BENACHITY+DURGAPUR%2C+WARD+NO.14%2C+West+Bengal%2C+India%2C+Code:19" target="_blank" rel="noopener"><i class="fa-solid fa-map" style="color:#c9a961;margin-right:.5rem"></i> View on Google Maps</a></p>
         </div>
       </div>
